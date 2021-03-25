a03499f00649d94d9a12a25df1c7cc7f9ea036cb
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-25 16:13:29 +0100

    print to screen

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //creare un array con i cognomi
 var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 console.log(cognomi);
-//chiedere il cognome alutente
-var userName = prompt('Inserisci il tuo cognome.').toLowerCase().trim();
+//chiedere il cognome all'utente
+var userName = prompt('Inserisci il tuo cognome.');
+while(userName === null || userName.trim ===''){
+    userName = prompt('Inserire un valore valido.');
+}
+userName = userName.trim();
 console.log(userName);
 //inserire il cognome del utente nella lista
 cognomi.push(userName);
@@ -10,3 +14,9 @@ console.log(cognomi);
 cognomi.sort();
 console.log(cognomi);
 console.log(cognomi.indexOf(userName) + 1);
+//print to screen
+var names = '';
+for(var i = 0; i < cognomi.length; i++) {
+    names = names + '<li>' + cognomi[i] + '</li>';
+}
+document.getElementById('lista').innerHTML = names;
